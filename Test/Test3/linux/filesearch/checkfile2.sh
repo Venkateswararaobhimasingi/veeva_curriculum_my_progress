63059a6510e9050f7a0d2f99cabd81a3427d8f99
Venkateswararaobhimasingi <srinu19773@gmail.com>
2026-02-25 10:29:00 +0530

    restructured

--- Test/Test3/linux/filesearch/checkfile2.sh
+++ Test/Test3/linux/filesearch/checkfile2.sh
@@ -2,8 +2,6 @@
 
 echo "Enter the file name to search:"
 read filename
-
-# Search from root directory, case-insensitive
 result=$(find / -type f -iname "$filename" 2>/dev/null)
 
 if [ "$result" ]; then
